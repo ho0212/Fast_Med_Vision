3ca3581cc9fa2848590746d066a119f68809383e
ho0212 <malone.ho@gmail.com>
2026-04-12 14:01:17 +1000

    add comment to specify img shape in each layer

--- unet.py
+++ unet.py
@@ -97,19 +97,19 @@ class UNet(nn.Module):
 
     def forward(self, x):
         # Encoder path
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
+        x1 = self.inc(x) # (8, 64, 240, 240)
+        x2 = self.down1(x1) # (8, 128, 120, 120)
+        x3 = self.down2(x2) # (8, 256, 60, 60)
+        x4 = self.down3(x3) # (8, 512, 30, 30)
 
         # Bottleneck
-        x5 = self.down4(x4)
+        x5 = self.down4(x4) # (8, 1024, 15, 15)
 
         # Decoder path with skip connections
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x = self.up1(x5, x4) # (8, 512, 30, 30)
+        x = self.up2(x, x3) # (8, 256, 60, 60)
+        x = self.up3(x, x2) # (8, 128, 120, 120)
+        x = self.up4(x, x1) # (8, 64, 240, 240)
 
         logits = self.outc(x)
         return logits
